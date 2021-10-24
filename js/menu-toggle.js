f2594356c47f8e7fe65544459f1d228e6f68cc58
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-10-23 18:51:25 -0700

    Debug menu toggle function

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -6,13 +6,13 @@ const mediaQueryMobile = window.matchMedia('(max-width: 425px)');
 const burgerButton = document.querySelector(".burger");
 const mobileMenu = document.querySelector(".menu");
 
-// function screenResize(e){
-//     if(e.matches){
-//         burgerButton.addEventListener('click', => {
-//             mobileMenu.classList.toggle("mobile-mode")
-//         });
-//     }
-// }
+function screenResize(e){
+    if(e.matches){
+        burgerButton.addEventListener('click', () => {
+            mobileMenu.classList.toggle("mobile-mode")
+        });
+    }
+}
 
 mediaQueryMobile.addListener(screenResize);
 
